23d87a20211a2f572a1db2819eb5b35df61ef673
Shahboss55 <rahulshah555666@gmail.com>
2024-05-22 13:33:52 +0530

    add some command

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
-# build_files.sh
-pip install -r requirements.txt
-#python3.9 manage.py migrate 
-python3.11 manage.py collectstatic --noinput
-
+echo "BUILD START"
+python3 -m ensurepip
+python3 -m pip install -r requirements.txt
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput --clear
+echo "BUILD END"
