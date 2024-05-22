f8745891661d6f90c263f23e4462cdc00660c7d4
Shahboss55 <rahulshah555666@gmail.com>
2024-05-22 15:45:41 +0530

    initail change

--- bossproject/settings.py
+++ bossproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = ('django-insecure-+om-+tiemv(ae3v@+sa3$yrm+1sr=3-q3hzw%y@vjbgb48snsl')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
